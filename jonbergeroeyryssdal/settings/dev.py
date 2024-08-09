25a759a2555eb83e483fedefc734933ed0348933
Jon Bergerøy Ryssdal <jon@ryssdal.net>
2024-08-09 13:55:41 +0200

    Initial commit

---
+++ jonbergeroeyryssdal/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-j_j%-fs5-%yg_7ji3x#t=s*ue2ac%7=_e!4er3=s4a%a4hypat"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
