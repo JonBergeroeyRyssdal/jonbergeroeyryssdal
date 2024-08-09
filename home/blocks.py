c08c6c63acca0506957db061578aa0f65841b036
Jon Bergerøy Ryssdal <jon@ryssdal.net>
2024-08-09 18:05:02 +0200

    before deployment

---
+++ home/blocks.py
@@ -0,0 +1,54 @@
+from wagtail import blocks
+from wagtail.images.blocks import ImageChooserBlock
+import uuid
+
+class BootstrapCardBlock(blocks.StructBlock):
+    id = blocks.CharBlock(
+        required=True,
+        help_text="Unique identifier for the card"
+    )
+    title = blocks.CharBlock(
+        required=True,
+        help_text="Add your card title"
+    )
+    text = blocks.TextBlock(
+        required=True,
+        help_text="Add your card text"
+    )
+    image = ImageChooserBlock(
+        required=True,
+        help_text="Add an image"
+    )
+    live_url = blocks.TextBlock(
+        required=False,
+        help_text="Add URL for live demo (optional)"
+    )
+    git_url = blocks.TextBlock(
+        required=True,
+        help_text="Add URL for GitHub repository"
+    )
+
+    class Meta:
+        template = "blocks/bootstrap_card.html"
+        icon = "doc-full"
+        label = "Bootstrap Card"
+
+class LanguageFrameworkBlock(blocks.StructBlock):
+    heading = blocks.CharBlock(required=True, max_length=100)
+    image = ImageChooserBlock(required=True)
+    description = blocks.TextBlock(required=True)
+    
+
+    class Meta:
+        template = "blocks/languages_frameworks.html"
+        icon = "doc-full"
+        label = "Language Framework"
+
+class SocialBlock(blocks.StructBlock):
+    logo = ImageChooserBlock(required=True)
+    url = blocks.URLBlock(required=True)  # Changed to URLBlock to ensure proper URL validation
+    
+    class Meta:
+        template = "blocks/social.html"
+        icon = "doc-full"
+        label = "Social media logo and link to profile"
--- home/models.py
+++ home/models.py
@@ -1,7 +1,62 @@
 from django.db import models
-
 from wagtail.models import Page
-
+from wagtail.admin.panels import FieldPanel, PageChooserPanel 
+from wagtail.fields import StreamField
+from .blocks import BootstrapCardBlock, LanguageFrameworkBlock, SocialBlock  # Import the block here
+from wagtail import blocks
 
 class HomePage(Page):
-    pass
+    head_title = models.CharField(max_length=100, default="Python Fullstack Web Developer")
+    head_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    about_text = StreamField([
+        ('paragraph', blocks.RichTextBlock()),
+    ], null=True)
+    about_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    languages_frameworks = StreamField([
+        ('languages_frameworks', LanguageFrameworkBlock()),
+    ], null=True, blank=True)
+    PDF_logo = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    CV = models.FileField(upload_to='documents/', blank=True, null=True
+                          )
+    project = StreamField([
+        ('card', BootstrapCardBlock()), 
+    ], null=True
+    )
+    social = StreamField([
+        ('social', SocialBlock()), 
+    ], null=True
+    )
+    footer_info = StreamField([
+        ('footer_info', blocks.TextBlock(template = "blocks/footer_info.html")),
+    ], null=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("head_title"),
+        PageChooserPanel("head_image"),
+        PageChooserPanel("about_image"),
+        FieldPanel("about_text"),
+        FieldPanel('languages_frameworks'),
+        PageChooserPanel("PDF_logo"),
+        FieldPanel('CV'),
+        FieldPanel('project'),
+        FieldPanel('social'),
+        FieldPanel('footer_info'),
+    ]
